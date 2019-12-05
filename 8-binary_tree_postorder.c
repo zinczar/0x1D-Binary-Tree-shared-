5721312e066aa2bdd426701e03912a9c35838a12
zinczar <brendan@wineisneat.com>
2019-12-05 22:58:08 +0000

    Betty fixes for 8-11

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_postorder - goes through a binary tree using post-order traversal
+ *binary_tree_postorder - goes through a binary tree using post-order traversal
  *@tree: binary_tree_t
  *@func: Int
  *
--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,9 +1,10 @@
 #include "binary_trees.h"
 
 /**
- * main - Code
+ * binary_tree_height - a function that measures the height of a binary tree
+ *@tree: binary_tree_t
  *
- * Return: 0
+ * Return: Longest side
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
@@ -12,15 +13,12 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	if (tree == NULL)
 		return (0);
+	if (tree->left)
+		left = binary_tree_height(tree->left) + 1;
+	if (tree->right)
+		right = binary_tree_height(tree->right) + 1;
+	if (left > right)
+		return (left);
 	else
-	{
-		if (tree->left)
-			left = binary_tree_height(tree->left) + 1;
-		if (tree->right)
-			right = binary_tree_height(tree->right) + 1;
-		if (left > right)
-			return (left);
-		else
-			return (right);
-	}
+		return (right);
 }
