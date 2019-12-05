9d36e9fe3aae2621cbeb5ebe4359388ad7fe4ff1
Hanh Nguyen <hanhuyeny2k@gmail.com>
2019-12-05 21:58:40 +0000

    malloc issue

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -9,10 +9,14 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *left_node = malloc(sizeof(binary_tree_t));
+	binary_tree_t *left_node;
 
-	if (!parent || !left_node)
+	if (!parent)
 	    return (NULL);
+	left_node = malloc(sizeof(binary_tree_t));
+
+	if (!left_node)
+		return (NULL):
 
         left_node->n= value;
         left_node->parent = parent;
