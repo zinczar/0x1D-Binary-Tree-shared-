3e3b5eeaba78b1566a7126f2b1834f86a24912e3
zinczar <brendan@wineisneat.com>
2019-12-06 22:03:56 +0000

    Advanced 1-5 setup

---
+++ 104-binary_tree_rotate_right.c
@@ -0,0 +1,14 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_rotate_right - performs a right-rotation on a binary tree
+ *@tree: binary_tree_t
+ *
+ * Return: Binary Tree Node
+ */
+binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree)
+{
+
+return (0);
+
+}
